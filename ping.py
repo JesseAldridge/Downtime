0f8247bb22e322f733d73aba01d9771a784fe767
Jesse Aldridge <JesseAldridge@gmail.com>
2012-01-21 17:11:43 -0600

    first

---
+++ ping.py
@@ -0,0 +1,25 @@
+
+import urllib2, os, time
+from datetime import datetime
+from os.path import exists
+
+if exists('downtime.txt'):  os.remove('downtime.txt')
+
+while True:
+    try:
+        response = urllib2.urlopen('http://google.com', timeout=1)
+        status = 'ok'
+    except urllib2.URLError:
+        status = 'down'
+    now = datetime.now()
+    minute = str(now.minute)
+    if len(minute) == 1:
+        minute = '0' + minute
+    date_str = "%i/%i/%i %i:%s %s" % (
+                now.month, now.day, now.year, now.hour % 12,
+                minute, 'AM' if now.hour < 12 else 'PM')
+    status_line = date_str + ' ' + status
+    print status_line
+    with open('downtime.txt', 'a') as f:
+        f.write(status_line + '\n')
+    time.sleep(60)
